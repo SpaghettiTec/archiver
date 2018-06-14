beb8143dd8b6e0efbd189254a0d0af4c68ab9a2b
Ivan Erofeev <i.eroffeev@yandex.ru>
2018-06-14 15:50:33 +0300

    Add files via upload

---
+++ Binary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Archive2._0.Utility
+{
+    class Binary
+    {
+        public static string ToBinConvert(char arg)
+        {
+            string ret;
+            ret = Convert.ToString(arg,2); //обычная функция получения бинарного кода
+            while(ret.Length <8)
+            {
+                ret = '0' + ret; //дополнение длины до 8
+            }
+            return ret;
+
+        }
+        public static string ToBinConvert(string arg)
+        {
+            string ret = "";
+            for (byte i =0; i<arg.Length;i++)
+            {
+                ret += ToBinConvert(arg[i]);
+            }
+            return ret;
+        }
+
+        public static char FromBinConvert(string arg)
+        {
+            int bt = 0;
+            for (byte i = 0; i < 8; i++)
+            {
+                if (arg[i] == '1')
+                {
+                    bt += (int)(Math.Pow(2, 7 - i));
+                }
+            }
+            return (char)(bt);
+        }
+    }
+}
